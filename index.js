ac8b2540f0bdaf4c0c1d5f64bb624390f2b4d268
Valerie Wood <valeriewd1@gmail.com>
2018-01-20 01:28:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ let getFirstSelector('div').;
 
 (getFirstSelector('div').id).toEqual('nested')
       expect(getFirstSelector('.ranked-list')).toEqual(document.querySelector('.ranked-list')
+      
+      let nestedTarget()
